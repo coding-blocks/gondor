80ef4bfe690ea9a3c1ee1bc600d280c4be680d2e
Naman Kumar <naman@outlook.in>
2020-03-26 02:53:10 +0530

    Fix graphql route

--- api/routes/graphql/index.js
+++ api/routes/graphql/index.js
@@ -2,8 +2,6 @@ import schema from 'Graphql/schema';
 import { ApolloServer } from 'apollo-server-micro';
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
   schema,
   context: ({ req }) => ({
     // TODO(naman): Integrate auth middleware
