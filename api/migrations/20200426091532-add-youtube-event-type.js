850ed0f123f57064f250100da067df714868bf52
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2020-04-26 16:03:06 +0530

    [Calendar Event Type] add youtube to calendar event type
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
    
    moved youtube above others
    
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

---
+++ api/migrations/20200426091532-add-youtube-event-type.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query(`
+      ALTER TYPE enum_calendar_events_type ADD VALUE 'Youtube';
+    `)
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query(`
+      
+    `)
+  }
+};
