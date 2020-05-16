60540b2826aaa23ab1fd6d57d49c319d4f62672e
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-05-16 15:59:35 +0530

    Expanding Calender Types: Fixes #39

--- app/components/ColorsList/index.js
+++ app/components/ColorsList/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './style.scss';
 
+const calenderMap = {
+  OfflineClass: 'Offline Class',
+  OnlineClass: 'Online Class',
+  CourseRecording: 'Course Recording',
+  OffSiteWorkshop: 'Off-Site Workshop',
+  Meeting: 'Meeting',
+  OOO: 'OOO',
+  Youtube: 'Youtube',
+  Other: 'Other',
+};
+
 const ColorsList = React.memo(({ colors }) => (
   <div className="row">
     {colors.map(({ name, color }) => (
       <div key={name} className="col-6 col-sm-12 mb-2 d-flex">
         <div className="color-box mr-2" style={{ backgroundColor: color }} />
-        <span>{name}</span>
+        <span>{calenderMap[name]}</span>
       </div>
     ))}
   </div>
