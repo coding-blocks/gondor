54a32ea3412abf6ef442b54e30279a2d634a6b29
prathambatra <batrapratham999@gmail.com>
2020-05-17 23:18:29 +0530

    Component width col8
    Signed-off-by: prathambatra <batrapratham999@gmail.com>

--- app/components/PageNotFound/index.js
+++ app/components/PageNotFound/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './style.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const PageNotFound = () => {
   return (
-    <div className="text-center">
+    <div className="col-8 col-centre text-center">
       <div className="heading">404</div>
       <h2>Page Not Found.</h2>
       <img src="/img/error-image.png" alt="404" />
--- app/pages/404.js
+++ app/pages/404.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import Navbar from 'Components/Navbar';
-import Sidebar from 'Components/Sidebar';
 import PageNotFound from 'Components/PageNotFound';
 
 const Custom404 = () => {
   return (
     <div>
-      <Navbar />
-      <Sidebar />
       <PageNotFound />
     </div>
   );
