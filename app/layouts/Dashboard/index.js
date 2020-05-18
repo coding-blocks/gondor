83923b016fe391265cfcc00c888a2e730a1d52e7
Shaurya Singhal <shauryasinghal84@gmail.com>
2020-05-18 10:01:26 +0530

    Hide Navbar and Sidebar based on query params fixes#56 (#61)

--- app/layouts/Dashboard/index.js
+++ app/layouts/Dashboard/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Navbar from 'Components/Navbar';
 import Sidebar from 'Components/Sidebar';
-
+import classNames from 'classnames';
+import { useRouter } from 'next/router';
 import './style.scss';
 
 const Dashboard = ({ children, data }) => {
+  const router = useRouter();
   if (!data) return null;
+
+  const isEmbed = router.query.hasOwnProperty('embed');
 
   return (
     <div className="dashboard-layout">
-      <Navbar />
-      <Sidebar />
-      <main>{children}</main>
+      {!isEmbed && (
+        <>
+          <Navbar />
+          <Sidebar />
+        </>
+      )}
+      <main className={classNames({ embed: isEmbed })}>{children}</main>
     </div>
   );
 };
