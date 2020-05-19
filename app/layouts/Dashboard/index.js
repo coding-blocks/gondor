4f2ede47ff4f9d627041fe63b1b2eae648c67147
Siddhant Sehgal <sehgal.siddhant999@gmail.com>
2020-05-20 00:19:36 +0530

    Fixed-conflicts

--- app/layouts/Dashboard/index.js
+++ app/layouts/Dashboard/index.js
@@ -2,9 +2,11 @@ import React, { useState } from 'react';
 import Navbar from 'Components/Navbar';
 import Sidebar from 'Components/Sidebar';
 import classNames from 'classnames';
+import { useRouter } from 'next/router';
 import './style.scss';
 
 const Dashboard = ({ children, data }) => {
+  const router = useRouter();
   if (!data) return null;
   const [showSideBar, setSideBar] = useState(true);
 
@@ -12,15 +14,21 @@ const Dashboard = ({ children, data }) => {
     setSideBar(!showSideBar);
   };
 
+  const isEmbed = router.query.hasOwnProperty('embed');
+
   return (
     <div
       className={classNames('dashboard-layout', {
         'menu-default menu-sub-hidden': showSideBar,
         'menu-default menu-sub-hidden main-hidden': !showSideBar,
       })}>
-      <Navbar toggleSideBar={toggleSideBar} />
-      <Sidebar show={showSideBar} />
-      <main>{children}</main>
+      {!isEmbed && (
+        <>
+          <Navbar toggleSideBar={toggleSideBar} />
+          <Sidebar show={showSideBar} />
+        </>
+      )}
+      <main className={classNames({ embed: isEmbed })}>{children}</main>
     </div>
   );
 };
