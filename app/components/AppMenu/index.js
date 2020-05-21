bd685dfe642e55b246efcc10a589cb6b321ed965
= <=>
2020-05-22 03:02:06 +0530

    Hide appmenu for non admins and members

--- app/components/AppMenu/index.js
+++ app/components/AppMenu/index.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import PerfectScrollbar from 'react-perfect-scrollbar';
+import useViewer from 'Hooks/useViewer';
 
 const Context = React.createContext({});
 
-const AppMenu = ({ children }) => (
-  <div className="app-menu">
-    <PerfectScrollbar
-      id="app-menu-scroll-container"
-      options={{ suppressScrollX: true, wheelPropagation: false }}>
-      <div className="scroll">
-        <div className="scrollbar-container overflow-auto">
-          <div className="p-4">
-            <Context.Provider value={{ target: 'app-menu-scroll-container' }}>
-              {children}
-            </Context.Provider>
+const AppMenu = ({ children }) => {
+  const viewer = useViewer();
+  return (
+    <div
+      className={`app-menu  ${
+        viewer.user.role === 'Admin' ? 'show-admin' : ''
+      }`}>
+      <PerfectScrollbar
+        id="app-menu-scroll-container"
+        options={{ suppressScrollX: true, wheelPropagation: false }}>
+        <div className="scroll">
+          <div className="scrollbar-container overflow-auto">
+            <div className="p-4">
+              <Context.Provider value={{ target: 'app-menu-scroll-container' }}>
+                {children}
+              </Context.Provider>
+            </div>
           </div>
         </div>
-      </div>
-    </PerfectScrollbar>
-  </div>
-);
+      </PerfectScrollbar>
+    </div>
+  );
+};
 
 AppMenu.Context = Context;
 
