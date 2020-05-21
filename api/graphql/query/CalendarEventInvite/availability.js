3d88fe311e0aa905e66fdaffb09ccf2931f0efeb
PrabalSingh <praba170107045@iitg.ac.in>
2020-05-22 01:31:12 +0530

    fix: ags -> _args

--- api/graphql/query/CalendarEventInvite/availability.js
+++ api/graphql/query/CalendarEventInvite/availability.js
@@ -1,6 +1,6 @@
 import Models from 'Models';
 
-const availability = async (parent, ags, { loaders }) => {
+const availability = async (parent, _args, { loaders }) => {
   const event = await Models.CalendarEvent.findByPk(parent.event_id);
 
   return loaders.userAvailabilityLoader.load({
