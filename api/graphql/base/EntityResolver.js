8f2760c590452362ddd1d6f66e670f08887676bb
prathambatra <batrapratham999@gmail.com>
2020-05-26 16:11:43 +0530

    Entity Resolver updated
    Signed-off-by: prathambatra <batrapratham999@gmail.com>

--- api/graphql/base/EntityResolver.js
+++ api/graphql/base/EntityResolver.js
@@ -1,11 +1,22 @@
 import BaseResolver from './Resolver';
+import Models from 'Models';
 
 class EntityResolver extends BaseResolver {
-  resolve = async () =>
-    this.model.findOne({
-      where: { id: Number(this.args.id) },
+  resolve = async () => {
+    const Op = Models.Sequelize.Op;
+    let where = { id: Number(this.args.id) };
+
+    if (this.model.rawAttributes.hasOwnProperty('slug')) {
+      where = {
+        [Op.or]: [{ id: Number(this.args.id) }, { slug: this.args.slug }],
+      };
+    }
+
+    return await this.model.findOne({
+      where: where,
       ...((await this.query?.call(this)) || {}),
     });
+  };
 }
 
 export default EntityResolver;
